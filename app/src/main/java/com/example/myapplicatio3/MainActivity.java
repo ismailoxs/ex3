10efb060ebef1944b9c201d452f3c667146eacfd
ismailoxs <139073609+ismailoxs@users.noreply.github.com>
2023-12-03 14:29:29 +0100

    button color finich

--- app/src/main/java/com/example/myapplicatio3/MainActivity.java
+++ app/src/main/java/com/example/myapplicatio3/MainActivity.java
@@ -10,12 +10,39 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public void chngeclorwhite(View view) {
+
+        View root = view.getRootView();
+
+
+        root.setBackgroundColor(Color.WHITE);
+    }
+    public void changecolorpink(View view) {
+
+        View root = view.getRootView();
+
+
+        root.setBackgroundColor(Color.parseColor("#F9C2FB"));
+    }
+    public void chngeclororange(View view) {
+
+        View root = view.getRootView();
+
+
+        root.setBackgroundColor(Color.parseColor("#F5CBA7"));
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button quiter=findViewById(R.id.button3);
+
+
+
+
+
         Button renitialiser=findViewById(R.id.button1);
         EditText editTextText2=findViewById(R.id.editTextText2);
         quiter.setOnClickListener(new View.OnClickListener() {
